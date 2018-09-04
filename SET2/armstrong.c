e000160a23cf557890682db6339a3738d0a0ca22
mathukumar <40791992+mathukumar@users.noreply.github.com>
2018-09-04 10:01:13 +0000

    Create armstrong.c

---
+++ SET2/armstrong.c
@@ -0,0 +1,34 @@
+    #include <stdio.h>
+    void main()
+
+    {
+
+        int number, sum = 0, rem = 0, cube = 0, temp;
+        scanf("%d", &number);
+
+        temp = number;
+
+        while (number != 0)
+
+        {
+
+            rem = number % 10;
+
+            cube = pow(rem, 3);
+
+            sum = sum + cube;
+
+            number = number / 10;
+
+        }
+
+        if (sum == temp)
+
+            printf ("yes");
+
+        else
+
+            printf ("no");
+
+    }
+
